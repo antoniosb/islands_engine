295b7ea9b6e6622d76ce44e66ba5c1745e0f178c
Antonio Britto <antonioabritto@gmail.com>
2017-08-14 01:51:26 -0300

    defining state: [:player1_turn, :player2_turn, :game_over] and actions: [:guess_coordinate, :win_check]

--- lib/islands_engine/rules.ex
+++ lib/islands_engine/rules.ex
@@ -8,7 +8,7 @@ defmodule IslandsEngine.Rules do
   def new(), do: %Rules{}
 
   def check(%Rules{state: :initialized} = rules, :add_player), do:
-  {:ok, %Rules{rules | state: :player_set}}
+  {:ok, %Rules{rules | state: :players_set}}
 
   def check(%Rules{state: :players_set} = rules, {:position_islands, player}) do
     case Map.fetch!(rules, player) do
@@ -22,6 +22,26 @@ defmodule IslandsEngine.Rules do
     case both_players_islands_set?(rules) do
       true -> {:ok, %Rules{rules | state: :player1_turn}}
       false -> {:ok, rules}
+    end
+  end
+
+  def check(%Rules{state: :player1_turn} = rules, {:guess_coordinate, :player1}), do:
+  {:ok, %Rules{rules | state: :player2_turn}}
+
+  def check(%Rules{state: :player1_turn} = rules, {:win_check, win_or_not}) do
+    case win_or_not do
+      :no_win -> {:ok, rules}
+      :win -> {:ok, %Rules{rules | state: :game_over}}
+    end
+  end
+
+  def check(%Rules{state: :player2_turn} = rules, {:guess_coordinate, :player2}), do:
+  {:ok, %Rules{rules | state: :player1_turn}}
+
+  def check(%Rules{state: :player2_turn} = rules, {:win_check, win_or_not}) do
+    case win_or_not do
+      :no_win -> {:ok, rules}
+      :win -> {:ok, %Rules{rules | state: :game_over}}
     end
   end
 
