d115720ae06748c1086a52c89a29645983037834
Antonio Britto <antonioabritto@gmail.com>
2017-08-13 23:06:26 -0300

    adding islands to board and checking if its all positioned

--- lib/islands_engine/board.ex
+++ lib/islands_engine/board.ex
@@ -1,3 +1,20 @@
 defmodule IslandsEngine.Boards do
+  alias IslandsEngine.Island
+
   def new(), do: %{}
+
+  def position_island(board, key, %Island{} = island) do
+    case overlaps_existing_island?(board, key, island) do
+      true -> {:erorr, :overlapping_island}
+      false -> Map.put(board, key, island)
+    end
+  end
+
+  def all_islands_positioned?(board), do: Enum.all?(Island.types, &(Map.has_key?(board, &1)))
+
+  defp overlaps_existing_island?(board, new_key, new_island) do
+    Enum.any?(board, fn {key, island} ->
+      key != new_key and Island.overlaps?(island, new_island)
+    end)
+  end
 end
