f13571d30d857bd1a5c54903ec8b7e59ad5dcf74
Antonio Britto <antonioabritto@gmail.com>
2017-08-13 21:53:26 -0300

    using "with" to model Island creation. Fixing typos

--- lib/islands_engine/island.ex
+++ lib/islands_engine/island.ex
@@ -4,22 +4,28 @@ defmodule IslandsEngine.Island do
   @enforce_keys [:coordinates, :hit_coordinates]
   defstruct [:coordinates, :hit_coordinates]
 
-  def new(), do:
-  %Island{coordinates: MapSet.new(), hit_coordinates: MapSet.new()}
+  def new(type, %Coordinate{} = upper_left) do
+    with [_|_] = offsets <- offsets(type), %MapSet{} = coordinates <- add_coordinates(offsets, upper_left)
+    do
+      {:ok, %Island{coordinates: coordinates, hit_coordinates: MapSet.new()}}
+    else
+      error -> error
+    end
+  end
 
   defp offsets(:square), do: [{0,0},{0,1},{1,0},{1,1}]
 
-  defp offset(:atol), do: [{0,0},{0,1},{1,1},{2,0},{2,1}]
+  defp offsets(:atol), do: [{0,0},{0,1},{1,1},{2,0},{2,1}]
 
-  defp offset(:dot), do: [{0,0}]
+  defp offsets(:dot), do: [{0,0}]
 
-  defp offset(:l_shape), do: [{0,0},{1,0},{2,0},{2,1}]
+  defp offsets(:l_shape), do: [{0,0},{1,0},{2,0},{2,1}]
 
-  defp offset(:s_shape), do: [{0,1},{0,2},{1,0},{1,1}]
+  defp offsets(:s_shape), do: [{0,1},{0,2},{1,0},{1,1}]
 
-  defp offset(_), do: {:error, :invalis_island_type}
+  defp offsets(_), do: {:error, :invalis_island_type}
 
-  defp add_coordiantes(offsets, upper_left) do
+  defp add_coordinates(offsets, upper_left) do
     Enum.reduce_while(offsets, MapSet.new(), fn offset, acc ->
       add_coordinate(acc, upper_left, offset)
     end)
